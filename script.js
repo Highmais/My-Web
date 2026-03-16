dc611c7062d75f77d0c6aea3bee2d8bc5b1af05b
Highmais <isaachighman740@gmail.com>
2026-03-16 15:11:55 -0400

    try this

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const button = document.getElementById('actionBtn');
+/*const button = document.getElementById('actionBtn');
 const message = document.getElementById('message');
 
 button.addEventListener('click', () => {
     message.textContent = "Button clicked! JavaScript is working.";
     message.style.color = "green";
     console.log("The interactive button was pressed!");
-});
+});*/
