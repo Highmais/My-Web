5a3ec7576e86d5c5db0cd1cc9e2b645f83e718f4
Highmais <isaachighman740@gmail.com>
2026-03-17 12:10:11 -0400

    try this

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ button.addEventListener('click', () => {
     message.style.color = "green";
     console.log("The interactive button was pressed!");
 });*/
+const myForm = document.getElementById('userForm');
+
+myForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData.entries());
+
+    console.log("Form Data Collected:", data);
+    alert(`Hello ${data.username} from ${data.location}!`);
+});
