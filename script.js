918dbecc7f861928b7fb97975cb7c7c1ec7a8b7d
Highmais <isaachighman740@gmail.com>
2026-03-20 10:24:39 -0400

    almost complete

--- script.js
+++ script.js
@@ -6,28 +6,54 @@ button.addEventListener('click', () => {
     message.style.color = "green";
     console.log("The interactive button was pressed!");
 });*/
-var answers = []
+const answers = [[1,13,15,7,9,10,2,8,14,11],[]]
 var locations = ["Backstretch","Barley Hopsters","Buns","Hamburger Inn","Oak and Brazen","Old Dog","Opa","Restoration","Roops","Solar Saloon","Son of Thurmans","Staas Brewing"]
-const myForm = document.getElementById('userForm');
+var myForm = document.getElementById('userForm');
+var formData;
+var data;
+var inputValues
 
 myForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    const formData = new FormData(this);
-    const data = Object.fromEntries(formData.entries());
+    formData = new FormData(this);
+    const inputElements = document.querySelectorAll('.input-field');
+    inputValues = Array.from(inputElements).map(input => input.value);
+    data = Object.fromEntries(formData.entries());
 
     //Send to google spreadsheet
     console.log("Form Data Collected:", data);
     //alerts browser window
-    alert(`Hello ${data.username} from ${data.location}!`);
+    doTheMath();
 });
 function doTheMath()
 {
     //Splice string to just have number, based on what number location check if list[location] == answer
     for(var i = 0;i < locations.length; i++)
     {
+        for()
         if(i == Number(data.location.slice(8)))    
         {
-            
+            console.log(Number(data.location.slice(8)));
+            //change to numbers in any order
+            /*
+            if(data.answer.length == answer[i].length){
+                var amt = 0;
+                for(var o=0; 0 < data.answer[i].length;o++)
+                {
+                    for(var p=0; p < answer[])
+                    {01
+
+                    }
+                }
+            }else
+            */
+            if(data.answer == answers[i])
+            {
+                alert("Guess was correct!")
+            }else
+            {
+                alert("Answer was incorrect")
+            }
         }
     }
 }
